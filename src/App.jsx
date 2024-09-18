738a34931f413fb8fe02ff0d2ee424ea2918b4e1
vineethkv7736 <vineethkv7736@gmail.com>
2024-09-19 01:27:25 +0530

    phases mobile view set

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <Info />
         <Clubs />
         <Speakers />
+        <Phases/>
         <Registration />
       </div>
       <Footer />
--- src/components/Pages/Phases.jsx
+++ src/components/Pages/Phases.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import { useState, useEffect } from "react";
+
 import { Button } from "@/components/ui/button";
 
-import onj from "../../assets/bg/phase.png";
 import {
   Card,
   CardContent,
@@ -17,64 +11,75 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+const array = [
+  { name: "DESIGN", phase: "03", option: "COMMING SOON", date: "." },
+  {
+    name: "IDEATHON",
+    phase: "01",
+    option: "REGISTER",
+    date: "28 SEPTEMBER 2024",
+  },
+  { name: "DEVELOPMENT", phase: "02", option: "COMMING SOON", date: "." },
+];
 
 const Phases = () => {
-  const array = [
-    { name: "DESIGN", phase: "03", option: "COMMING SOON", date: "." },
-    {
-      name: "IDEATHON",
-      phase: "01",
-      option: "REGISTER",
-      date: "28 SEPTEMBER 2024",
-    },
-    { name: "DEVELOPMENT", phase: "02", option: "COMMING SOON", date: "." },
-  ];
+  const [isMobile, setIsMobile] = useState(false);
+
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); 
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  
+  const sortedArray = isMobile
+    ? [...array].sort((a, b) => a.phase - b.phase)
+    : array; 
+
+  
   return (
-    <div className="h-screen flex flex-col items-center">
+    <div className=" flex flex-col items-center gap-5">
       <h1 className=" text-4xl">Phases of Lumino</h1>
 
-      <Carousel className="my-10 ">
-        <CarouselContent className="p-10">
-          {array.map((obj, index) => (
-            <CarouselItem
-              key={index}
-              className="md:basis-1/2 lg:basis-1/3 px-3"
+      <div className="flex flex-col md:flex-row my-10">
+        {sortedArray.map((obj, index) => (
+          <div
+            key={index}
+            className="md:basis-1/2 lg:basis-1/3 px-3 flex  flex-col"
+          >
+            <Card
+              className={` mt-10 flex flex-col justify-center items-center  phase border-none transform transition-transform duration-300 md:${
+                index === 1 ? "scale-110" : ""
+              }`}
             >
-              {/* <div className="">
-                <img src={onj} alt="qwe" className=" relative" />
-                <div className="absolute top-1/4 left-1/2 flex flex-col justify-center items-center">
-                  <h1 className="text-3xl">Phase</h1>
-                  <h1>01</h1>
-                </div>
-              </div> */}
-              <Card
-                className={`flex flex-col justify-center items-center  phase border-none transform transition-transform duration-300 ${
-                  index === 1 ? "scale-110" : ""
-                }`}
-              >
-                <CardHeader className=" flex flex-col justify-center items-center text-5xl py-12">
-                  <CardTitle>PHASE</CardTitle>
-                  <CardDescription className="text-4xl">
-                    {obj.phase}
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="text-2xl font-bold py-3">
-                  <p>{obj.name}</p>
-                </CardContent>
-                <CardFooter className="flex flex-col justify-center items-center rounded-lg   pt-5 ">
-                  <p className="text-xs mb-2 ">{obj.date}</p>
-                  <Button
-                    variant="outline"
-                    className="w-56 rounded-xl border-none  bg-white/40 text-black"
-                  >
-                    {obj.option}
-                  </Button>
-                </CardFooter>
-              </Card>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+              <CardHeader className=" flex flex-col justify-center items-center text-5xl py-12">
+                <CardTitle>PHASE</CardTitle>
+                <CardDescription className="text-4xl">
+                  {obj.phase}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="text-2xl font-bold py-3">
+                <p>{obj.name}</p>
+              </CardContent>
+              <CardFooter className="flex flex-col justify-center items-center rounded-lg   pt-5 ">
+                <p className="text-xs mb-2 ">{obj.date}</p>
+                <Button
+                  variant="outline"
+                  className="w-56 rounded-xl border-none  bg-white/40 text-black"
+                >
+                  {obj.option}
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
