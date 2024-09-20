328bc36d0de23d542ea8072cc830c6990cf96068
niranjan-official <niranjansabarinath1521@gmail.com>
2024-09-20 17:32:39 +0530

    new reg form added

--- src/components/Pages/Registration.jsx
+++ src/components/Pages/Registration.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
 
 const Registration = () => {
   return (
@@ -24,28 +27,53 @@ const Registration = () => {
           </div>
         </div>
         <div className="w-full md:w-1/2 flex justify-center">
-          <div className="w-full md:w-3/4 lg:w-1/2 p-5 shadow shadow-neutral-500 reg-bg pb-6">
-            <p className="text-lg text-neutral-400">Enter your credentials</p>
-            <form className="flex flex-col items-start gap-6 mt-8" action="">
-              <input type="text" className="w-full" placeholder="Name" />
-              <input type="text" className="w-full" placeholder="Email" />
-              <input type="text" className="w-full" placeholder="Phone" />
-              <button className="bg-black border border-blue-400 rounded-3xl p-1 px-3">
-                Select phase
+          <div className="w-full md:w-3/4 lg:w-1/2 p-5 border border-neutral-500 rounded-2xl z-50 bg-black">
+            <h2 className="font-bold text-xl text-neutral-200">
+              Register to Lumino
+            </h2>
+            <form className="mt-8 mb-4">
+              <LabelInputContainer className="mb-4 z-50">
+                <Label htmlFor="firstname">Full Name</Label>
+                <Input id="firstname" placeholder="Tyler" type="text" />
+              </LabelInputContainer>
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="email">Email Address</Label>
+                <Input
+                  id="email"
+                  placeholder="projectmayhem@fc.com"
+                  type="email"
+                />
+              </LabelInputContainer>
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="phone">Phone No</Label>
+                <Input
+                  id="number"
+                  placeholder="+91 9876543222"
+                  type="numeric"
+                />
+              </LabelInputContainer>
+
+              <button
+                className="relative bg-zinc-900 w-full text-white rounded-[0.4rem] mt-4 h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                type="submit"
+              >
+                Register Now &rarr;
               </button>
-              <div className="w-full flex justify-end">
-                <button
-                  type="submit"
-                  className="p-2 bg-green-600 rounded-[0.4rem] px-5 arial-font text-lg"
-                >
-                  Register
-                </button>
-              </div>
+
+              <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent mt-8 h-[1px] w-full" />
             </form>
           </div>
         </div>
       </div>
     </section>
+  );
+};
+
+const LabelInputContainer = ({ children, className }) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
   );
 };
 
